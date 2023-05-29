2a5ba814480de0c94f6aa2d3dc057f699c4978ef
VRNarasimhun <127402339+VRNarasimhun@users.noreply.github.com>
2023-05-29 23:43:36 +0530

    Update HotelController.cs

--- HotelAPI/Controllers/HotelController.cs
+++ HotelAPI/Controllers/HotelController.cs
@@ -1,40 +1,144 @@
-﻿using HotelAPI.Models;
-using HotelAPI.Services;
+
+using HotelAPI.Models;
+using HotelManagementAPI.Interfaces;
+using HotelManagementAPI.Models;
+using HotelManagementAPI.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
-namespace HotelAPI.Controllers
+namespace HotelManagementAPI.Controllers
 {
     [Route("api/[controller]/[action]")]
     [ApiController]
     public class HotelController : ControllerBase
     {
-        private readonly HotelService _hotelService;
+        private readonly IRepo<int, Hotel> _repo;
+        private readonly HotelService _hotelservice;
 
-        public HotelController(HotelService hotelService)
+        public HotelController(IRepo<int, Hotel> repo, HotelService hotelservice)
         {
-            _hotelService = hotelService;
-        }
-        [ProducesResponseType(typeof(Hotel), StatusCodes.Status200OK)]//Success Response
-        [ProducesResponseType(StatusCodes.Status404NotFound)]//Failure Response
-        [HttpPost]
-        public ActionResult<Hotel> Add_Hotel(Hotel hotel)
-        {
-            var myHotel = _hotelService.AddHotel(hotel);
-            if (myHotel != null)
-                return Created("Hotel created Successfully", myHotel);
-            return BadRequest(new Error(1, "Unable to add Hotel"));
+            _repo = repo;
+            _hotelservice = hotelservice;
+
         }
 
-        [ProducesResponseType(typeof(Rooms), StatusCodes.Status200OK)]//Success Response
-        [ProducesResponseType(StatusCodes.Status404NotFound)]//Failure Response
-        [HttpPost]
-        public ActionResult<Rooms> Add_Room(Rooms room)
+        [Authorize]
+        [HttpGet]
+        [ProducesResponseType(typeof(List<Hotel>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult<List<Hotel>> FetchAllHotels()
         {
-            var myroom = _hotelService.AddRoom(room);
-            if (myroom != null)
-                return Created("Room created Successfully", myroom);
-            return BadRequest(new Error(2, "Unable to add Room"));
+            var hotels = _repo.GetAll();
+            if (hotels.Count == 0)
+            {
+                return NotFound("No Hotels  available");
+            }
+            return Ok(hotels);
+
         }
+
+        [Authorize]
+        [HttpGet]
+        [ProducesResponseType(typeof(Hotel), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult<Hotel> Get(int key)
+        {
+            Hotel hotels = _repo.Get(key);
+            if (hotels != null)
+                return Ok(hotels);
+            return NotFound("No hotel available");
+        }
+
+
+
+
+
+        [Authorize]
+        [HttpGet]
+        [ProducesResponseType(typeof(List<Hotel>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult FetchAllHotelsBasedOnLocation(string location)
+        {
+            var hotels = _hotelservice.GetHotelsByLocation(location);
+            if (hotels.Count == 0)
+            {
+                return NotFound("No Hotels  available");
+            }
+            return Ok(hotels);
+
+        }
+
+
+
+        [Authorize]
+
+        [HttpGet]
+        [ProducesResponseType(typeof(List<Hotel>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult<List<Hotel>> FetchHotelsWithinAPriceRange(int min, int max)
+        {
+            var hotels = _hotelservice.GetHotelsByPrice(min, max);
+            if (hotels.Count == 0)
+            {
+                return NotFound("No Hotels  available");
+            }
+            return Ok(hotels);
+
+        }
+
+        [Authorize]
+
+
+
+
+        
+
+
+
+
+        [Authorize(Roles = "admin")]
+
+        [HttpPost]
+        public ActionResult<Hotel> Post(Hotel hotel)
+        {
+            Hotel prod = _repo.Add(hotel);
+            return Created("HotelListing", prod);
+        }
+        [Authorize(Roles = "admin")]
+
+        [HttpDelete]
+        [ProducesResponseType(typeof(Hotel), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public ActionResult<Hotel> Delete(int id)
+        {
+            var hotel = _repo.Delete(id);
+            if (hotel != null)
+            {
+                return Ok(hotel);
+            }
+            return BadRequest("Unable to delete the hotel");
+        }
+
+        [Authorize(Roles = "admin")]
+
+        [HttpPut]
+        [ProducesResponseType(typeof(Hotel), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public ActionResult<Hotel> Update(Hotel hotel)
+        {
+            var updatedHotel = _repo.Update(hotel);
+            if (updatedHotel == null)
+            {
+                BadRequest("Unable to update hotel details");
+            }
+            return Ok(hotel);
+        }
+
+
+
+
+
     }
+
 }
